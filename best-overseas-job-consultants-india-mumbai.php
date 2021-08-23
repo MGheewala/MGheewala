e2c5126706fcfe592a84d37366367117de88d75e
MGheewala <89388873+MGheewala@users.noreply.github.com>
2021-08-23 16:04:46 +0530

    Add files via upload

---
+++ best-overseas-job-consultants-india-mumbai.php
@@ -0,0 +1,285 @@
+﻿<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+    <title>Best Overseas job Consultants in India, Mumbai | M Gheewala </title>
+    <meta name="description" content="M Gheewala an Overseas Job Consultants in India for gulf jobs Government approved recruitment agency in India, Overseas Recruitment Consultants In India, Overseas Recruitment Agencies In Mumbai." >
+	
+		
+	<meta name="keywords" content="Best Overseas Job Consultants In India, overseas job consultants in India, overseas jobs consultants in mumbai, Overseas Recruitment Consultants,  Overseas Recruitment Agencies In Mumbai, Overseas Placement Agency">
+	<!-- Open Graph general (Facebook, Pinterest & Google+) -->
+<meta name="og:title" content="Best Overseas job Consultants in India, Mumbai | M Gheewala ">
+<meta name="og:description" content="M Gheewala an Overseas Job Consultants in India for gulf jobs Government approved recruitment agency in India, Overseas Recruitment Consultants In India, Overseas Recruitment Agencies In Mumbai.">
+<meta name="og:image" content="https://www.mgheewala.com/assets/img/logo_hd.png">
+<meta name="og:url" content="https://www.mgheewala.com/">
+<meta name="og:site_name" content="MGheewala">
+<meta name="og:video" content="https://www.youtube.com/channel/UCTrortdqZyfqLemhN8ZK-NQ">
+<meta name="fb:admins" content="665268926895420">
+<meta name="fb:app_id" content="1870649719925073">
+<meta name="pinterest:admins" content="882344731928412">
+<meta name="og:type" content="website">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@mgheewala_hrc">
+<meta name="twitter:title" content="Best Overseas job Consultants in India, Mumbai | M Gheewala ">
+<meta name="twitter:description" content="M Gheewala an Overseas Job Consultants in India for gulf jobs Government approved recruitment agency in India, Overseas Recruitment Consultants In India, Overseas Recruitment Agencies In Mumbai.">
+
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport"/>
+    <link href="images/favicon.png" rel="shortcut icon"/>
+    <link rel="shortcut icon" href="images/favicon.png" type="image/png" />
+    <!-- JS FILES -->
+    <script type="text/javascript" src="assets/js/jquery-1.20.2.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+    <!-- CSS FILES -->
+    <link href="assets/css/style.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="assets/css/responsive.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="assets/rs-plugin/css/settings.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="assets/css/navstylechange.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="assets/css/cubeportfolio.min.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="#" media="screen" rel="stylesheet" type="text/css" id="style-schem-color"> 
+    <link href="#" media="screen" rel="stylesheet" type="text/css" id="style-schem"> 
+
+<style type="text/css">
+.values-column {
+    display: table;
+    margin: 10px 0px 0px 0px;
+}.values-column-col {
+    display: table-cell;
+    padding: 0px;
+    vertical-align: top;
+    padding: 15px;
+    background: #eeeeee;
+	width: 180px;
+}
+.values-column-col1 {
+    padding-left: 10px;
+}
+.value-left-head {
+    font-size: 14px;
+}
+.hr1 {
+    border: 1px #e3e1e1 solid;
+    border-width: 0px 0px 1px 0px;
+    margin: 10px 0px 10px 0px;
+    width: 100%;
+}
+ul.accordin-careere li {
+    list-style: circle;
+    margin: 0px 0px 0px 5px;
+    margin: 0px 0px 10px 0px;
+    line-height: 20px;
+}
+.assistance {
+    padding: 5% 5%;
+	
+    margin: 0px;
+    margin-top: 20px;
+    float: left;
+    background: #eeeeee;
+    border-radius: 8px;
+    border: 1px solid #eeeeee;
+    border-top: 3px solid #eeeeee;
+    color: #000;
+	    margin-bottom: 25px;
+}.assistance .pic-assis {
+    text-align: right;
+    float: right;
+    margin-left: 10px;
+}.assistance .number {
+    font-size: 16px;
+}.assistance a {
+    font-size: 15px;
+}h5 {
+    font-size: 17px;
+    color: #000;
+    font-weight: normal;
+    margin-bottom: 10px;
+    padding: 0px;
+    margin-top: 5px;
+}
+</style>
+    <!-- Start Scroll to top Jquery script  -->
+    <script type="text/javascript">
+        $(document).ready(function () {
+
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 100) {
+                    $('.scrollup').fadeIn();
+                } else {
+                    $('.scrollup').fadeOut();
+                }
+            });
+
+            $('.scrollup').click(function () {
+                $("html, body").animate({ scrollTop: 0 }, 600);
+                return false;
+            });
+
+        });
+
+   </script>
+    <!-- Ends Scroll to top Jquery script  -->
+    <!-- Start Fade In Jquery script  -->
+
+    <script type="text/javascript">
+        jQuery(document).ready(function ($) {
+            $("body").delay(400).animate({ "opacity": "1" }, 1000);
+        });
+    </script>
+    <style type="text/css">
+        body
+        {
+         opacity: 0;
+        }
+    </style>
+
+    <!-- End Fade In Jquery script  -->
+
+   
+      
+    
+    <!-- Textbox Trim Script //-->
+    <script type="text/javascript" src="js/trim.js"></script>
+    <!-- Textbox Trim Script //-->
+
+    <!-- Accordian Script //-->
+    <link href="css/accordion.css" rel="stylesheet" type="text/css" />
+
+<?php include 'google_analytical.php' ;?>	
+
+  </head>
+  <body>
+   <?php include 'header.php';?>
+	  
+      <div class="page-in">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 pull-left">
+			<div class="page-in-name" style="font-size:23px">BEST OVERSEAS JOB CONSULTANTS INDIA MUMBAI</div></div>
+            <div class="col-lg-6 pull-right"><div class="page-in-bread">
+			<ul>
+                    <li class="home" style="color:#fff;line-height: 16pt;font-size: 13px;"><a href="index.php">
+                        <img src="images/home.png" alt="recruitment service  in mumbai" />&nbsp;&nbsp;</a><i class="fa fa-angle-right" style="color:#fff;"></i>
+				 <a href="https://www.mgheewala.com">About Us</a>
+                 </li>
+                    </ul>
+</div></div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="container marg0">
+        <div class="row">
+          <div class="col-lg-12">
+          </div> 
+        </div>
+      </div>  
+      <div class="container marg100" style="margin-top:30px;">
+        <div class="row">
+          <div class="col-lg-9 col-md-9 col-sm-12">            
+            <div class="marg50" style="margin-top: 0px; ">
+              <div class="row">
+
+  
+<div class="promo-block">
+<h1 class="promo-text" style="color: #1E4F9B;font-weight: 500;padding: 0px;margin-bottom: 10px;">Best overseas job consultants in India  </h1>
+</div>			
+
+<p>The core aim of our agency has always been to ensure that clients worldwide are given human resources based on their needs and requirements. Having been a direct serving agency in the overseas job marker and carrying a wealth of experience in this field, M Gheewala Global, HR Consultants have been ranked as an outstanding performer. It will not be out of place to mention that we are considered  as one of the best overseas job consultants in India. </p>
+
+<p>Innovation has been the key strengths and has been ingrained as a management philosophy thus making every employee imbibe and inculcate the same. In fact the vision and mission of the organisation is completely shared by every employee which results in the impeccable deliveries it has been delivering for years now. Actually our longevity in the field of overseas recruitments, especially in the middle-east and the gulf sector, speaks volumes of our dedication and uncompromising commitment to ensure that the client gets exactly what he wants and the only way he wants. Simultaneously we also ensure that the candidates to get their dream jobs based on their skills sets and expertise.</p>
+
+<p>Words like - excellence, integrity, relationship, commitment, guidance, expertise, are easily associated by the job seekers. The candidates know where they are heading and are at ease when they get a call from M Gheewala HR Consultants. For them we are easily one of the best overseas job consulting firms in India.</p>
+
+
+<div class="promo-block">
+<h2 class="promo-text" style="color: #1E4F9B;font-weight: 500;padding: 0px;margin-bottom: 10px;">M Gheewala Overseas consultancy Mumbai </h2>
+</div>	
+
+<p>M Gheewala HR Consultants has provided a platform for both employers and employees over four decades. Based out of Mumbai we have ensured that we have gone from strength to strength. Today as one of the most renowned, highly valued and respected recruiting agency and HR solutions provider, young prospective employees have our organisation on their lips when they have to  recommend someone for a job abroad.  And this reputation we have earned based on our physical practices and visible results. However it is not just in India but also among all the corporations in the Gulf and Middle-East who vote for us when it comes to delivering HR Solutions for them. It has been our experience that often we get direct enquiries from overseas organizations who faithfully put forward their requests based on their past experience. And we deliver in accordance with their wishes. </p>
+
+<p>That the client is king is a policy we have adopted from inception. That we exist because of them is the truth. Hence we have delivered on all job fronts virtually. So whether it is : construction, operations & maintenance; oil& gas; engineering & project management; engineering procurement installation construction (EPC); power & utility; IT & telecommunications; Manufacturing; commerce & retail; hospitality; teaching & education administration AND healthcare, we are best equipped to handle complete recruitment for each of these.</p>
+
+<p>We have a solid base of professionals who come with the right education as well as behavioural qualities. Then the skilled and semi-skilled employee base too is readily available while the search for more and more is happening all the time.  Matching the comfort of the jobseeker and the job giver is our motto which is never compromised at any time. The idea is to create a long lasting alliance to the mutual benefit of both of them.</p>
+
+<p>Our organisation has the right mix of experts and young turks which makes the team members highly well balances. So with speed, you get expertise and efficiency and results which are always timely. With multitasking as the forte, candidates are handled in a one-contact manner.</p>
+
+<p>With user-friendly portal, that allows candidates to apply online, the waiting period is almost negligible. The members are also in constant touch with the Indian authorities in order to gain an insight into all the latest developments which are then appraised to the corporate and the candidates too.</p>
+
+<p>M Gheewala HR Solutions is suitably located in the commercial heart and hub of India - Mumbai. While it also has its recruiting offices and associates in metros two and three tier cities. There are international tie-ups too which make us a widespread and reliable entity. Our state of the art server network keeps track of the database of all categories and levels staring from the top to the general workforce for a host of industries already mentioned above.</p>
+
+<p>Our success story, that of becoming one of the best overseas job consultants in India is solely because our management is actively participative in the day to day proceedings and is solely focussed on  delivering as per the client’s demands. Today we have come to a stage where the first name for overseas recruitments is taken as Gheewala.</p>
+
+			  
+          
+              </div>
+			  
+            </div>
+			
+			
+		
+          </div>
+		
+
+    <div class="col-lg-3 col-md-3 col-sm-12">
+
+           <img alt="Best Overseas job Consultants in India, Mumbai | M Gheewala" src="images/consultant.jpg" style="width:100%">
+
+            <div class="marg50" style="margin-top: 10px;">
+
+			
+
+                <ul style="line-height: 30px;" >
+
+        <li style="border-bottom: 1px dashed #cccccc;" ><a href="our-strengths.php"><i class="fa fa-angle-right"></i>&nbsp;Our Strengths</a></li>
+
+        <li style="border-bottom: 1px dashed #cccccc;"><a href="mission-vision-values.php"><i class="fa fa-angle-right"></i>&nbsp;Mission, Vision and Values</a></li>
+
+        <li style="border-bottom: 1px dashed #cccccc;"><a href="management-team.php"><i class="fa fa-angle-right"></i>&nbsp;Management Team</a></li>
+
+        <li style="border-bottom: 1px dashed #cccccc;"><a href="our-presence.php"><i class="fa fa-angle-right"></i>&nbsp;Our Presence</a></li>
+
+        <li style="border-bottom: 1px dashed #cccccc;"><a href="careers-at-mgheewala-global.php"><i class="fa fa-angle-right"></i>&nbsp;Careers at M. Gheewala Global</a></li>
+
+        <li style="border-bottom: 1px dashed #cccccc;"><a href="profile/m-gheewala-profile.pdf"><i class="fa fa-angle-right"></i>&nbsp;Download Company Profile</a></li>
+
+    </ul>
+
+
+
+            </div>
+
+          </div>
+
+		
+        </div>
+      </div>
+	
+            
+             
+	<?php include 'footer.php';?>
+    </div>  
+    <script type="text/javascript" src="assets/rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+    <script type="text/javascript" src="assets/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="assets/js/waypoints.min.js"></script>
+    <script type="text/javascript" src="assets/js/sticky.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.tweet.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.flexslider-min.js"></script>
+    <script type="text/javascript" src="assets/js/retina.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.cycle.all.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.cubeportfolio.min.js"></script>
+    <script type="text/javascript" src="assets/js/portfolio-main.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.cookie.js"></script>
+    <script type="text/javascript" src="assets/js/jcarousel.responsive.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.jcarousel.min.js"></script>
+    <script type="text/javascript" src="assets/js/switch.class.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.dlmenu.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+
+
+  
+  </body>
+</html>
